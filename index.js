b6df1de6d274b653a9000a331a1b553e33a969fc
Nitin-Kumar22 <nitinthainua800@gmail.com>
2026-01-15 14:36:08 +0530

    basic code

---
+++ index.js
@@ -0,0 +1,13 @@
+//1.hello world
+console.log("Node.js Backend Started");   // print the message information to the console so developer understand
+
+//2.variables
+let name ="aman";   //String   declare a variable in a string
+let age ="22";      //number    declare a variable in a number
+const country ="India";     //constant  declare a variable in a string but data is secure
+let isStudent = true;       //boolean      declare a boolean data type to output true or false
+
+console.log("name",name);           // print the message information string  variable
+console.log("age",age);                  // print the message information  number variable
+console.log("country",country);              // print the message information  constant variable
+console.log("isStudent",isStudent);              // print the message information boolean data
